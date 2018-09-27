ea888ae1273424f97695904000b7d2280e182e8b
Chris <chrismorrisrva@gmail.com>
2018-09-27 01:11:53 -0400

    Before the overhaul

--- Pizza.cs
+++ Pizza.cs
@@ -4,12 +4,39 @@ namespace assignemntThree
 {
 	class Pizza
 	{
-		//const int small = 5;
-		//const int large = 8;
-		// int Eaters;
-		// string chris;
-		// string rafay;
-		// string darren;
+		  int smallPrice = 10; // instance variable 
+		  int largePrice = 16; 
+		  int numPizza;
+			
+		  Pizza(int numPizza){
+			  this.numPizza = numPizza;
+		  }
+
+		  
+
+
+
+
+		public static void DisplayTitle()
+		{
+            Console.WriteLine("Pizza calculator: Presented by chris morris");
+		}
+		
+		public static double getInput(string x)
+		{
+			string userInN;
+			Console.WriteLine("how many pizzas do you want " + x + "?");
+			userInN = Console.ReadLine();
+			double pizzaNum = double.Parse(userInN);
+            return pizzaNum;
+		}
+		public static string inputDiameter(string x)
+		{
+			string pizzaChoice;
+			Console.WriteLine("You can order either a 'small' or 'large', What size would you like your pizza to be " + x +"? (Only 1 size per order).");
+			pizzaChoice = Console.ReadLine();
+			return pizzaChoice;
+		}
 
 		public static void Main()
 		{
@@ -17,6 +44,8 @@ namespace assignemntThree
 		   double numPizza = getInput("chris");
 		   string pizzaSize = inputDiameter("chris");
 		   Console.ReadKey();
+		   
+		  		   
 		  // double numPizza2 = getInput('rafay');
 		  // double pizzaSize2 = inputDiameter('rafay');
 		  // double numPizza3 = getInput('darren');
@@ -26,33 +55,25 @@ namespace assignemntThree
 		  // displayResults(allEaters);
 
 		}
-		public static void DisplayTitle()
+		
+		
+		
+		/* public static double calculateCost(double x, string y)
 		{
-            Console.WriteLine("Pizza calculator: Presented by chris morris");
+			if (y == "small") 
+			{
+				return smallPrice*x;
+			} 
+			else if (y == "large") 
+			{
+				return largePrice*x;
+			}
 		}
-		public static double getInput(string x)
-		{
-			string userInN;
-			Console.WriteLine("how many pizzas do you want " + x + " ?");
-			userInN = Console.ReadLine();
-			double pizzaNum = double.Parse(userInN);
-            return pizzaNum; // or  return double.Parse(inputValue);
-		}
-		public static string inputDiameter(string x)
-		{
-			string pizzaChoice;
-			Console.WriteLine("You can order either a 'small' or 'large', What size would you like your pizza to be " + x +"? (Only 1 size per order).");
-			// Console.WriteLine("if desiring more information on sizes of the pizza enter 'sizes'");
-			pizzaChoice = Console.ReadLine();
-			return pizzaChoice;
-		}
-		/*public static double calculateEaters(double )
+
+		public static double calculateEaters(double )
 		{}
 		public static void displayResults(double x)
-		{
-			Console.WriteLine("The diameter of all of the pizzas for togeather is " + totalDiameter + " in inches.");
-			//Console.WriteLine("The number of eaters will be " + allEaters + ".");
-		}*/
+		{}*/
 
 	}
 }
